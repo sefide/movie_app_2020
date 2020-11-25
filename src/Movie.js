b9e2b3e8b4bc717b51e360a7c0b62e38a9abd73c
sefide <sefide@naver.com>
2020-11-25 22:29:42 +0900

    style: add css

--- src/Movie.js
+++ src/Movie.js
@@ -9,9 +9,9 @@ function Movie({year, title, summary, poster, genres}) {
     <div className="movie">
         <img src={poster} alt={title} title={title}/>
         <div className="movies_movie">
-            <h3 className="movie__title" style={{backgroundColor: "beige"}}>{title}</h3>
+            <h3 className="movie__title">{title}</h3>
             <h5 className="movie__year">{year}</h5>
-            <ul className="genres"> 
+            <ul className="movie__genres"> 
                 {genres.map((genre, index) => 
                     <li className="movie__genre" key={index}> {genre}</li>)}
             </ul>
